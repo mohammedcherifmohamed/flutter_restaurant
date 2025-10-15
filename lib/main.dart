a6bb0b3d2adad8a65b1e8c52aed64e517f57ddb0
Mohammed-Cherif Mohamed <mafsamarari@gmail.com>
2025-10-15 21:18:15 +0200

    Scroll column and some styles

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
           )
 
         ),
-        body: Container(
+        body: SingleChildScrollView(
             //width:400,
             //height:80,
             //color:Colors.orange,
@@ -169,6 +169,172 @@ class MyApp extends StatelessWidget {
 
                 ),
 
+
+              Container(
+                margin: EdgeInsets.all(10),
+                child:Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:[
+                    // ___ IMAGE _______
+                    Container(
+                      height: 200,
+                      decoration : BoxDecoration(
+                      borderRadius: BorderRadius.only(  topLeft: Radius.circular(20),   // top-left corner radius
+                        topRight: Radius.circular(20)),
+                        image:DecorationImage(
+                          image: AssetImage('images/ecole_6.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+
+                      ),
+                     ),
+                    // ________ TILTE
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.all(10),
+                      color:Colors.grey,
+
+                      child:Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('FOOD TITLE',style:TextStyle(fontWeight: FontWeight.bold,color:Colors.deepOrange)),
+                          Text("FOOD DESCRIPTION",style:TextStyle(color:Colors.grey)),
+                        ],
+                      ),
+                    ),
+
+                    //__________________ PRICE AND TIME AND RATE
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.all(10),
+
+                      child :Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children:[
+                            Text('600 DA'),
+                            Text('35 min'),
+                            Text('3.5'),
+                          ],
+
+                          ),
+
+
+
+                    ),
+
+                  ],
+                    ),
+              ),
+              Container(
+                color:Colors.blueGrey,
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(10),
+                child:Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:[
+                    // ___ IMAGE _______
+                    Container(
+                      height: 200,
+                      decoration : BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image:DecorationImage(
+                          image: AssetImage('images/ecole_6.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+
+                      ),
+                    ),
+                    // ________ TILTE
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.all(10),
+
+                      child:Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('FOOD TITLE',style:TextStyle(fontWeight: FontWeight.bold,color:Colors.deepOrange)),
+                          Text("FOOD DESCRIPTION",style:TextStyle(color:Colors.grey)),
+                        ],
+                      ),
+                    ),
+
+                    //__________________ PRICE AND TIME AND RATE
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.all(10),
+
+                      child :Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children:[
+                          Text('Price'),
+                          Text('Time'),
+                          Text('Rate'),
+                        ],
+
+                      ),
+
+
+
+                    ),
+
+                  ],
+                ),
+              ),
+              Container(
+                color:Colors.blueGrey,
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(10),
+                child:Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:[
+                    // ___ IMAGE _______
+                    Container(
+                      height: 200,
+                      decoration : BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image:DecorationImage(
+                          image: AssetImage('images/ecole_6.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+
+                      ),
+                    ),
+                    // ________ TILTE
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.all(10),
+
+                      child:Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('FOOD TITLE',style:TextStyle(fontWeight: FontWeight.bold,color:Colors.deepOrange)),
+                          Text("FOOD DESCRIPTION",style:TextStyle(color:Colors.grey)),
+                        ],
+                      ),
+                    ),
+
+                    //__________________ PRICE AND TIME AND RATE
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.all(10),
+
+                      child :Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children:[
+                          Text('Price'),
+                          Text('Time'),
+                          Text('Rate'),
+                        ],
+
+                      ),
+
+
+
+                    ),
+
+                  ],
+                ),
+              ),
 
             ],
 
